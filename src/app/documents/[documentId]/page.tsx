52287480b0ca753a1bf13f9d17c28c4f64494a9a
oguz <oguz.oeztuerk@outlook.de>
2025-06-26 23:57:15 +0200

    Add UI primitive components with Radix UI and custom styles.

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>
+}
+
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+
+  return (
+    <div>
+      DocumentIdPage: {documentId}
+    </div>
+  );
+};
+
+export default DocumentIdPage;
---
+++ src/app/documents/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface DocumentsLayoutProps {
+  children: React.ReactNode;
+}
+
+const DocumentsLayout = ({ children }: DocumentsLayoutProps) => {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <nav className="w-full">Document navbar</nav>
+      {children}
+    </div>
+  );
+};
+
+export default DocumentsLayout;
